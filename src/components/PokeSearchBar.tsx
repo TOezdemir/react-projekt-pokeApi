cc80ba49c97d8ad14752fb08be1ba5dfcb8c8e8c
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-10-23 11:37:39 +0200

    api error handling, searchbar

---
+++ src/components/PokeSearchBar.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+import type { Pokemon } from "../lib/api";
+import getPokemonById from "../lib/api";
+
+
+export default function PokeSearchBar(){
+	const [pokemonData, setPokemonData] = useState<Pokemon | void>(null);
+    const [searchText, setSearchText] = useState("")
+	const inputRef = useRef<HTMLInputElement>(null)
+
+    const catchPokemon  = async () => {
+        const searchText = inputRef.current?.value
+        if(searchText){
+            setPokemonData(await getPokemonById(searchText));
+            inputRef.current!.value = ""
+        }
+    }
+
+	useEffect(() => {
+		catchPokemon();
+	}, []);
+
+    return(
+        <section>
+            <form action="">
+            <input 
+              				type="text" 
+              				aria-label="Pokemon search field" 
+              				placeholder="Pokemon"
+							ref={inputRef}
+              				className="border border-gray-400 px-3 py-2 rounded-lg mr-2"
+              				onChange={(event) => setSearchText(event.target.value)}
+					/>
+            </form>
+        </section>
+    )
+}
