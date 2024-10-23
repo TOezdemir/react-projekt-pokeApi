f7f7cfa93ef2758a7bef7e90e31c764e0ebf717e
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-10-23 10:23:19 +0200

    5th commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// Wir instantiieren einen neuen QueryClient. Hier haben wir die Moeglichkeit, globale Einstellungen vorzunehmen.
+// (Muessen wir aber nicht)
+const queryClient = new QueryClient()
+// const queryClient = new QueryClient({defaultOptions: {queries: {refetchOnWindowFocus: false}}})
+
+// React Query braucht, vergleichbar mit React Router, einen Provider der um die gesamte App gewickelt wird
+// (Das kann in index.tsx oder App.tsx passieren, sollte aber moeglichst weit "oben" sein)
+// Dieser Provider wird genutzt um Requests zu speichern und zu deduplizieren, aber auch um globale Einstellungen vorzunehmen
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
