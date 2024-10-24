aa5978a2bf98eec1af1d3ffbeacb0c228ecaa33a
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-10-24 14:33:01 +0200

    deleted line

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -24,21 +24,6 @@ const TypePage = () => (
   </div>
 );
 
-// mit Array(number) koennen wir einen Array mit einer bestimmten laenge erzeugen:
-// const array151 = Array(151)
-// dieser wird allerdings leere Eintraege haben.
-// Also nicht [undefined, undefined, undefined,...]
-// sondern [empty x 151] (es handelt sich um einen sogenannten "sparse array")
-// Manche Operationen funktionieren damit, manche aber auch nicht.
-// array.map z.B. funktioniert nicht, [...array] oder Array.fill() aber schon.
-// Bevor wir mappen koennen, muessen wir den array also erst in eine Form zwingen,
-// in der seine Eintraege nicht mehr empty, sondern zumindest undefined sind.
-// Das geht z.B. so: Array(151).fill(1)
-// (Hier sind alle Eintraege 1)
-// oder auch so: [...Array(151)]
-// (hier sind alle Eintraege undefined) 
-// Erst dann koennen wir mappen.
-
 const emptyPokemonArray = Array(151).fill(1);
 
 const Homepage = () => {
@@ -59,8 +44,6 @@ const Homepage = () => {
 
         <div>
           {emptyPokemonArray.map((_id, index) => (
-            // da alle eintraege des Arrays 1 sind, verwenden wir den index um hochzuzaehlen.
-            // da der index bei 0 anfaengt, addieren wir jeweils 1
             <PokemonCard key={index} id={index + 1} />
           ))}
         </div>
